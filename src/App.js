51736675feacb97d50c9d1a656b7ad2e234b387d
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-15 19:46:57 -0500

    Apply another changes to fix the compilation issue

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Books from './pages/Books';
 import Categories from './pages/Categories';
+import './App.css';
 
 function App() {
   return (
--- src/components/book/Book.jsx
+++ src/components/book/Book.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
+// eslint-disable-next-line react/prop-types
 function Book({ title, author }) {
   return (
     <div className="container-book">
       <div className="info-book">
-        <h4 className='category-book'>Category</h4>
-        <h2 className='title-book'>{title}</h2>
-        <h4 className='author-book'>{author}</h4>
+        <h4 className="category-book">Category</h4>
+        <h2 className="title-book">{title}</h2>
+        <h4 className="author-book">{author}</h4>
       </div>
       <div className="actions-book">
-        <button type="button" className='btn btn-primary'>Comments</button>
-        <button type="button" className='btn btn-primary'>Remove</button>
-        <button type="button" className='btn btn-primary'>Edit</button>
+        <button type="button" className="btn btn-primary">Comments</button>
+        <button type="button" className="btn btn-primary">Remove</button>
+        <button type="button" className="btn btn-primary">Edit</button>
       </div>
     </div>
   );
