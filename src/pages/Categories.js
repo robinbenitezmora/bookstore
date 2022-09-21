a6e058ed7baf1e689f6a26c5f00592f4b2d39eb7
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-21 09:43:52 -0500

    Add code lines in 'Categories.js'

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
-function Categories() {
+const Categories = () => {
+  const dispatch = useDispatch();
+  const handleFilter = (e) => {
+    dispatch(e.target.value);
+  };
+
   return (
-    <button type="button">Check Status</button>
+    <button type="button" className="btn btn-primary" onClick={handleFilter}>Check Status</button>
   );
-}
+};
 
 export default Categories;
