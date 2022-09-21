d1dc0c47192ff613138579f23f6a0b89d6086216
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-21 18:01:19 -0500

    Refactor 'Categories.js' pages

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
-  const category = useSelector((state) => state.categories);
   const dispatch = useDispatch();
-
-  const handleCheckStatus = () => {
-    dispatch(checkStatus(category));
-  };
-
   return (
-    <div className="container-categories">
-      <button type="button" className="btn btn-primary" onClick={handleCheckStatus}>Check Status</button>
-      <p>{category}</p>
-    </div>
+    <button type="button" onClick={() => dispatch(checkStatus())}>Check Status</button>
   );
 };
 
 export default Categories;
+
+// const Categories = () => {
+//   const category = useSelector((state) => state.categories);
+//   const dispatch = useDispatch();
+
+//   const handleCheckStatus = () => {
+//     dispatch(checkStatus(category));
+//   };
+
+//   return (
+//     <div className="container-categories">
+//       <button type="button" className="btn btn-primary" onClick={handleCheckStatus}>Check Status</button>
+//       <p>{category}</p>
+//     </div>
+//   );
+// };
+
+// export default Categories;
