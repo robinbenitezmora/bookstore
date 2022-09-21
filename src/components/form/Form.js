6bedb1a7300f681977dae6febfbb212b2f411c12
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-21 07:50:22 -0500

    Add code lines in 'Form.js' in order to add new books

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/books/books';
 
 function Form() {
+  const [inputText, setInputText] = useState({ title: '', author: '' });
+  const dispatch = useDispatch();
+
+  const onChange = (e) => {
+    setInputText({ ...inputText, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title: inputText.title,
+      author: inputText.author,
+    };
+
+    dispatch(addBook(newBook));
+    setInputText({ title: '', author: '' });
+  };
+
   return (
     <div className="container-form">
-      <h2 className="title-form">ADD A NEW BOOK</h2>
-      <form className="form" action="#">
-        <input type="text" className="title" placeholder="Title" />
-        <input type="text" className="author" placeholder="Author" />
+      <h2 className="title-form">ADD NEW BOOK</h2>
+      <form onSubmit={submitHandler}>
+        <input type="text" name="title" value={inputText.title} onChange={onChange} placeholder="Book title" />
+        <input type="text" name="author" value={inputText.author} onChange={onChange} placeholder="Author" />
         <button type="submit" className="btn btn-primary">Add Book</button>
       </form>
     </div>
--- src/components/library/Book-list.js
+++ src/components/library/Book-list.js
@@ -1,24 +1,10 @@
 import React from 'react';
+import { shallowEqual, useSelector } from 'react-redux';
 import Book from '../book/Book';
 
 function BookList() {
-  const books = [
-    {
-      id: 1,
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: 2,
-      title: 'Dune',
-      author: 'Frank Herbert',
-    },
-    {
-      id: 3,
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-    },
-  ];
+  const books = useSelector((state) => state.books, shallowEqual);
+
   return (
     <div className="container-book-list">
       <ul className="book-list">
