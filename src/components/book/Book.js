f5dac1757db895cf28cac9a7067b2ac3b0771d75
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-21 16:26:36 -0500

    Refactor 'Book.js' component

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -4,24 +4,19 @@ import PropTypes from 'prop-types';
 import { removeBook } from '../../redux/books/books';
 
 const Book = (props) => {
+  const [itemId, title, author, category] = props;
   const dispatch = useDispatch();
-  const { book } = props;
-  const { id, title, author } = book;
-
-  const handleRemoveBook = () => {
-    dispatch(removeBook(id));
-  };
 
   return (
     <div className="container-book">
-      <div key={id} className="book">
+      <div className="book">
         <div className="book-info">
-          <p className="book-category">Category</p>
+          <p className="book-category">{category}</p>
           <h3 className="book-title">{title}</h3>
           <p className="book-author">{author}</p>
           <div className="book-actions">
             <button type="button" className="btn btn-primary">Comments</button>
-            <button type="button" className="btn btn-primary" onClick={handleRemoveBook}>Remove</button>
+            <button type="button" className="btn btn-primary" onClick={() => dispatch(removeBook(itemId))}>Remove</button>
             <button type="button" className="btn btn-primary">Edit</button>
           </div>
         </div>
@@ -37,6 +32,7 @@ Book.defaultProps = {
     id: '',
     title: '',
     author: '',
+    category: '',
   },
 };
 
@@ -45,5 +41,6 @@ Book.propTypes = {
     id: PropTypes.string,
     title: PropTypes.string,
     author: PropTypes.string,
+    category: PropTypes.string,
   }),
 };
