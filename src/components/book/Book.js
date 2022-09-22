4ea44a7cb120c168c1ebe988ebd5eadf8d056dff
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-22 11:51:28 -0500

    Correct syntaxis errors in 'Book.js' file

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -11,7 +11,11 @@ function Book({ book }) {
       <div className="book-info">
         <p className="book-category">{book.category}</p>
         <h2 className="book-title">{book.title}</h2>
-        <p className="book-author">Author</p>
+        <p className="book-author">
+          Author:
+          {' '}
+          {book.author}
+        </p>
         <div className="book-buttons">
           <button type="button" className="book-button">Comments</button>
           <button type="button" className="book-button" onClick={() => dispatch(removeBook(book))}>Remove</button>
@@ -32,45 +36,3 @@ Book.propTypes = {
 };
 
 export default Book;
-
-// const Book = (props) => {
-//   const [itemId, title, author, category] = props;
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="container-book">
-//       <div className="book">
-//         <div className="book-info">
-//           <p className="book-category">{category}</p>
-//           <h3 className="book-title">{title}</h3>
-//           <p className="book-author">{author}</p>
-//           <div className="book-actions">
-//             <button type="button" className="btn btn-primary">Comments</button>
-//             <button type="button" className="btn btn-primary" onClick={() => dispatch(removeBook(itemId))}>Remove</button>
-//             <button type="button" className="btn btn-primary">Edit</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Book;
-
-// Book.defaultProps = {
-//   book: {
-//     id: '',
-//     title: '',
-//     author: '',
-//     category: '',
-//   },
-// };
-
-// Book.propTypes = {
-//   book: PropTypes.shape({
-//     id: PropTypes.string,
-//     title: PropTypes.string,
-//     author: PropTypes.string,
-//     category: PropTypes.string,
-//   }),
-// };
